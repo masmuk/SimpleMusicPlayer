d2daf39a03d237e0468ea9b55c2b2036f7d35bca
ratwareid <jerryerlangga82@gmail.com>
2020-01-11 11:36:41 +0700

    watermark

--- app/src/main/java/com/erlangga/smplayer/core/android/vanilla/PreferencesActivity.java
+++ app/src/main/java/com/erlangga/smplayer/core/android/vanilla/PreferencesActivity.java
@@ -247,7 +247,7 @@ public class PreferencesActivity extends PreferenceActivity
 			super.onCreate(savedInstanceState);
 
 			Activity activity = getActivity();
-			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/vanilla-music/vanilla-music.github.io/wiki"));
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ratwareid/SimpleMusicPlayer/"));
 			if (intent != null) {
 				startActivity(intent);
 			}
